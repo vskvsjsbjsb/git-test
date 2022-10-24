b812df241f3cfc3371dda5e68ea0e17b86883288
yuxuyang <3095960746@qq.com>
2022-10-24 15:43:49 +0800

    four commit second hot-fix

--- git-test/src/main/java/Git_test.java
+++ git-test/src/main/java/Git_test.java
@@ -1,9 +1,11 @@
 public class Git_test {
     public static void main(String[] args) {
         int id =100/25;
+        String name ="zhangsan";
         System.out.println("hello world");
         System.out.println("hello world2");
         System.out.println("hello world3");
         System.out.println(id);
+        System.out.println(name);
     }
 }
