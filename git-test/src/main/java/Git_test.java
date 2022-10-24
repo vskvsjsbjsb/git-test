e9cab7c00dec3be7d6c6fbb22d0077801b2a7615
yuxuyang <3095960746@qq.com>
2022-10-24 15:41:52 +0800

    four commit 6

--- git-test/src/main/java/Git_test.java
+++ git-test/src/main/java/Git_test.java
@@ -1,9 +1,11 @@
 public class Git_test {
     public static void main(String[] args) {
         int id =100/25;
+        int ig = 10000/25;
         System.out.println("hello world");
         System.out.println("hello world2");
         System.out.println("hello world3");
         System.out.println(id);
+        System.out.println(ig);
     }
 }
